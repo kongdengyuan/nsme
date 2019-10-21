412a0f57aa0c1b042be76b8a2834a05cc85f72cc
kongdengyuan <kongdengyuan@gmail.com>
2019-10-21 16:51:53 +0800

    ...

--- install_docker.sh
+++ install_docker.sh
@@ -38,6 +38,7 @@ docker-compose -v >&/dev/null
 if [ $? -eq 0 ];then 
    echo "docker-compose already install"
  else 
+   echo "Begin to install docker-compose"
    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && echo "docker-compose install success"
 fi 
 }
